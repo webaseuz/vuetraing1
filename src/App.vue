898c39ace5ef700305561383d9d58cdfb1b53bb9
Bahodir Hojiyev <bahodir.hojiyev2607@gmail.com>
2020-07-09 22:11:19 +0500

    dsd

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <h1>
+      Hello World
+    </h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+  #app{
+    display: flex;
+    width: 100%;
+    height: 100vh;
+    font-family: Verdana,serif;
+  }
+  h1{
+    width: 100%;
+    display: flex;
+    text-align: center;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
